f808cfd00c5560e6e022962619271aaf791af12b
URA, Takefumi <ura@spirals.co.jp>
2014-09-16 13:34:41 +0900

    language and coding system

--- init.el
+++ init.el
@@ -3,3 +3,7 @@
 ;; 非常に重要な設定
 (global-set-key "\C-h" 'backward-delete-char-untabify)
 
+;; 文字コード
+(set-language-environment 'utf-8)
+(prefer-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8-unix)

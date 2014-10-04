d1c9afe9949bb7e3dcdfd09f4e824351ce91af2a
URA, Takefumi <ura@spirals.co.jp>
2014-10-04 20:54:37 +0900

    some settings for helm

--- init.el
+++ init.el
@@ -87,11 +87,41 @@
 ;;;
 (define-key isearch-mode-map (kbd "C-h") 'isearch-delete-char)
 
+
 ;;;
 ;;; Helm
 ;;;
 (when (require 'helm-config nil t)
-  (global-set-key "\C-\M-o" 'helm-mini))
+  (global-set-key "\C-\M-o" 'helm-mini)
+  (global-set-key "\M-x" 'helm-M-x)
+  (global-set-key "\C-xb" 'helm-buffers-list)
+  (global-set-key "\C-x\C-f" 'helm-find-files)
+
+  (define-key helm-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+  (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
+
+  ;; Emulate `kill-line' in helm minibuffer
+  (setq helm-delete-minibuffer-contents-from-point t)
+  (defadvice helm-delete-minibuffer-contents (before helm-emulate-kill-line activate)
+    "Emulate `kill-line' in helm minibuffer"
+    (kill-new (buffer-substring (point) (field-end))))
+
+  ;; Correct ambiguous file name filtering
+  (defadvice helm-ff-transform-fname-for-completion (around my-transform activate)
+    "Transform the pattern to reflect my intention"
+    (let* ((pattern (ad-get-arg 0))
+	   (input-pattern (file-name-nondirectory pattern))
+	   (dirname (file-name-directory pattern)))
+      (setq input-pattern (replace-regexp-in-string "\\." "\\\\." input-pattern))
+      (setq ad-return-value
+	    (concat dirname
+		    (if (string-match "^\\^" input-pattern)
+			;; '^' is a pattern for basename
+			;; and not required because the directory name is prepended
+			(substring input-pattern 1)
+		      (concat ".*" input-pattern))))))
+  )
 
 
 ;;;

5f6ef2109f3a950214acace1e68792c24a5abad5
URA, Takefumi <ura@spirals.co.jp>
2014-10-21 06:11:44 +0900

    magit関連のバッファをpopwinに

--- init.el
+++ init.el
@@ -169,6 +169,7 @@
   (when (featurep 'helm-config)
     (setq helm-samewindow nil)
     (push '("*compilatoin*" :noselect t) popwin:special-display-config)
+    (push '("\\*magit.*\\*" :regexp t) popwin:special-display-config)
     (push '("helm" :regexp t :height 0.4) popwin:special-display-config))
 
   ;; フレームのサイズに応じてpopwinの出現位置を決める

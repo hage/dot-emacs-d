d363d107e264a9e37c8282d530b88a160c5b7d25
URA, Takefumi <ura@spirals.co.jp>
2014-11-14 11:08:52 +0900

    ruby-modeをロード時の変数を設定した

--- init.el
+++ init.el
@@ -473,8 +473,11 @@
 (when (autoload-if-found 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
   (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
   (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+  (eval-after-load "ruby-mode"
+    #'(progn
+	(setq ruby-indent-level 2)
+	(setq ruby-indent-tabs-mode nil)))
   )
-
 
 ;;;
 ;;; php-mode

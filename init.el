f48b9c42ccdfdfe3950a9cdf8f070cedde8f5131
URA, Takefumi <ura@spirals.co.jp>
2014-11-15 14:31:27 +0900

    fix a pretty bug

--- init.el
+++ init.el
@@ -496,8 +496,8 @@
   (eval-after-load 'auto-complete
     #'(add-to-list 'ac-modes 'inf-ruby-mode))
   (add-hook 'inf-ruby-mode-hook 'ac-inf-ruby-enable)
-  (eval-after-load 'inf-ruby '
-    '(define-key inf-ruby-mode-map (kbd "TAB") 'auto-complete)))
+  (eval-after-load 'inf-ruby
+    #'(define-key inf-ruby-mode-map (kbd "TAB") 'auto-complete)))
 
 
 ;;;

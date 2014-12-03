6405f853cf6738c04c9885b74f3c84f01e9b61b1
URA, Takefumi <ura@spirals.co.jp>
2014-12-03 23:01:33 +0900

    alignの設定が間違っていたので修正した

--- init.el
+++ init.el
@@ -518,26 +518,28 @@
 	  (global-rinari-mode t))
 	))
   ;; align
-  (add-to-list 'align-rules-list
-	       '(ruby-comma-delimiter
-		 (regexp . ",\\(\\s-*\\)[^# \t\n]")
-		 (repeat . t)
-		 (modes  . '(ruby-mode))))
-  (add-to-list 'align-rules-list
-	       '(ruby-hash-literal
-		 (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
-		 (repeat . t)
-		 (modes  . '(ruby-mode))))
-  (add-to-list 'align-rules-list
-	       '(ruby-assignment-literal
-		 (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
-		 (repeat . t)
-		 (modes  . '(ruby-mode))))
-  (add-to-list 'align-rules-list	;TODO add to rcodetools.el
-	       '(ruby-xmpfilter-mark
-		 (regexp . "\\(\\s-*\\)# => [^#\t\n]")
-		 (repeat . nil)
-		 (modes  . '(ruby-mode))))
+  (eval-after-load "align"
+    #'(progn
+	(add-to-list 'align-rules-list
+		     '(ruby-comma-delimiter
+		       (regexp . ",\\(\\s-*\\)[^# \t\n]")
+		       (repeat . t)
+		       (modes  . '(ruby-mode))))
+	(add-to-list 'align-rules-list
+		     '(ruby-hash-literal
+		       (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
+		       (repeat . t)
+		       (modes  . '(ruby-mode))))
+	(add-to-list 'align-rules-list
+		     '(ruby-assignment-literal
+		       (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
+		       (repeat . t)
+		       (modes  . '(ruby-mode))))
+	(add-to-list 'align-rules-list	;TODO add to rcodetools.el
+		     '(ruby-xmpfilter-mark
+		       (regexp . "\\(\\s-*\\)# => [^#\t\n]")
+		       (repeat . nil)
+		       (modes  . '(ruby-mode))))))
   )
 ;;   robeを使うには
 ;;     Gemfileに 'pry' と記述しておいて M-x robe-start

3eb5570c817f68be73a6421c93380be824a5f23d
URA, Takefumi <ura@spirals.co.jp>
2014-12-03 19:37:59 +0900

    add align rules for ruby-mode

--- init.el
+++ init.el
@@ -517,6 +517,27 @@
 	(when (require 'rinari nil t)
 	  (global-rinari-mode t))
 	))
+  ;; align
+  (add-to-list 'align-rules-list
+	       '(ruby-comma-delimiter
+		 (regexp . ",\\(\\s-*\\)[^# \t\n]")
+		 (repeat . t)
+		 (modes  . '(ruby-mode))))
+  (add-to-list 'align-rules-list
+	       '(ruby-hash-literal
+		 (regexp . "\\(\\s-*\\)=>\\s-*[^# \t\n]")
+		 (repeat . t)
+		 (modes  . '(ruby-mode))))
+  (add-to-list 'align-rules-list
+	       '(ruby-assignment-literal
+		 (regexp . "\\(\\s-*\\)=\\s-*[^# \t\n]")
+		 (repeat . t)
+		 (modes  . '(ruby-mode))))
+  (add-to-list 'align-rules-list	;TODO add to rcodetools.el
+	       '(ruby-xmpfilter-mark
+		 (regexp . "\\(\\s-*\\)# => [^#\t\n]")
+		 (repeat . nil)
+		 (modes  . '(ruby-mode))))
   )
 ;;   robeを使うには
 ;;     Gemfileに 'pry' と記述しておいて M-x robe-start

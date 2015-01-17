e6215dad5c3989999034c3d7622e990c0b938a3c
URA, Takefumi <ura@spirals.co.jp>
2015-01-17 15:48:50 +0900

    setup basic haskell-mode

--- init.el
+++ init.el
@@ -685,6 +685,18 @@
 
 
 ;;;
+;;; haskell-mode
+;;;
+(eval-after-load "haskell-mode"
+  (lambda ()
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+    (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+    (add-hook 'haskell-mode-hook 'font-lock-mode)
+    (add-hook 'haskell-mode-hook 'imenu-add-menubar-index))
+)
+
+
+;;;
 ;;; e2wm
 ;;;
 (when (autoload-if-found 'e2wm:start-management "e2wm" nil t)

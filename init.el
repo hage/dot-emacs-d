ae125229a551b482b9b83368a8b4b6dd92caf944
URA, Takefumi <ura@spirals.co.jp>
2015-05-06 20:53:53 +0900

    ファイル名が README.md のとき gfm-mode (GitHub Flavored Markup) になるようにした

--- init.el
+++ init.el
@@ -755,6 +755,7 @@
   (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
   (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
   )
 
 ;;;

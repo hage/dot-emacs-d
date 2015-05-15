250220a3a86af476f271f98fe15fce289167ab2d
URA, Takefumi <ura@spirals.co.jp>
2015-05-15 15:47:41 +0900

    vcを使わないように設定した

--- init.el
+++ init.el
@@ -145,6 +145,13 @@
           (lambda ()
             (delete-trailing-whitespace)))
 
+;; vcを起動しないようにする
+(custom-set-variables
+ '(vc-handled-backends nil))
+;; vcが当てた不要なhookを外す
+(remove-hook 'find-file-hook 'vc-find-file-hook)
+(remove-hook 'kill-buffer-hook 'vc-kill-buffer-hook)
+
 ;;;
 ;;; キー・バインドの変更、新規割当
 ;;;

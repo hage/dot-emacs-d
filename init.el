bcf72d3042976f2494751e655c2cf0b99367e42e
URA, Takefumi <ura.takefumi@gmail.com>
2016-01-27 12:42:31 +0900

    helmにくくりつけのmdfind検索をやめてlocateっぽい動作をするmdfindに

--- init.el
+++ init.el
@@ -505,13 +505,20 @@ Otherwise indent whole buffer."
 	(set-face-foreground 'helm-source-header "yellowgreen")
 	(set-face-underline 'helm-source-header t)
 	(set-face-foreground 'helm-match "hotpink1")
+        (setq helm-locate-command
+              (case system-type
+                ('gnu/linux "locate -i -r %s")
+                ('berkeley-unix "locate -i %s")
+                ('windows-nt "es %s")
+                ('darwin "mdfind -name %s %s")
+                (t "locate %s"))
+              )
 
         (custom-set-variables
          '(helm-mini-default-sources `(helm-source-buffers-list
                                        helm-source-ls-git
                                        helm-source-recentf
-                                       helm-source-findutils
-                                       ,(if osxp 'helm-source-mac-spotlight)
+                                       helm-source-locate
                                        )))
 	))
   (eval-after-load "helm-files"

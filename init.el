03965dd75d24a762a4f4a80fe8bea17b18ea8229
URA Takefumi <tura@caesar.local>
2016-04-14 14:38:21 +0900

    diff face

--- init.el
+++ init.el
@@ -1579,3 +1579,6 @@ Otherwise sends the current line."
 (set-face-foreground 'font-lock-type-face "LimeGreen")
 (set-face-background 'secondary-selection "MediumPurple4")
 (set-face-foreground 'font-lock-function-name-face "MediumTurquoise")
+(set-face-foreground 'diff-removed "black")
+(set-face-foreground 'diff-added "black")
+(set-face-foreground 'diff-context "gray60")

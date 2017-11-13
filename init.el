2432d8e3b26817c5f93e47ea65156871744a50f8
URA Takefumi <ura.takefumi@gmail.com>
2017-11-13 13:49:44 +0900

    enhancement mc/insert-numbers
    cf. https://qiita.com/ShingoFukuyama/items/3ad7e24cb2d8f55b4cc5

--- init.el
+++ init.el
@@ -820,6 +820,32 @@ Otherwise indent whole buffer."
 	   (autoload-if-found 'mc/edit-lines "multiple-cursors" nil t))
   (global-set-key (kbd "C-q C-SPC") 'mc/mark-all-dwim)
   (global-set-key (kbd "C-x r t") 'mc/edit-lines)
+
+  (eval-after-load "multiple-cursors"
+    (lambda ()
+      ;; cf. https://qiita.com/ShingoFukuyama/items/3ad7e24cb2d8f55b4cc5
+      ;; insert specific serial number
+      (defvar my/mc/insert-numbers-hist nil)
+      (defvar my/mc/insert-numbers-inc 1)
+      (defvar my/mc/insert-numbers-pad "%01d")
+
+      (defun my/mc/insert-numbers (start inc pad)
+        "Insert increasing numbers for each cursor specifically."
+        (interactive
+         (list (read-number "Start from: " 0)
+               (read-number "Increment by: " 1)
+               (read-string "Padding (%01d): " nil my/mc/insert-numbers-hist "%01d")))
+        (setq mc--insert-numbers-number start)
+        (setq my/mc/insert-numbers-inc inc)
+        (setq my/mc/insert-numbers-pad pad)
+        (mc/for-each-cursor-ordered
+         (mc/execute-command-for-fake-cursor
+          'my/mc--insert-number-and-increase
+          cursor)))
+      (defun my/mc--insert-number-and-increase ()
+        (interactive)
+        (insert (format my/mc/insert-numbers-pad mc--insert-numbers-number))
+        (setq mc--insert-numbers-number (+ mc--insert-numbers-number my/mc/insert-numbers-inc)))))
   )
 
 ;;;

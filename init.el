ddb8e8aa9d57a00f5e9b5bb5452939accb598b3a
URA Takefumi <ura.takefumi@gmail.com>
2018-03-13 00:55:20 +0900

    tmux の window-name 設定を最初に持ってきた
    なんか emacs が設定するっぽいやつが「前の window-name」に設定されてし
    まい、Emacs を終了したら "Emacs-25.3" とかいう window-name になってい
    たので修正した

--- init.el
+++ init.el
@@ -14,6 +14,28 @@
 ;; (initchart-record-execution-time-of require feature)
 ;;;;;;;;;;;;;;;;
 
+;;; tmux 内にいる && Emacs がインタラクティブに起動 && "Emacs" が window-name にないとき
+;;; window-name を設定する。
+;;; また、Emacs 終了時に window-name を起動時のものに戻す。
+(defvar my-saved-tmux-window-name nil)
+(let ((case-fold-search nil)            ; case-sensitive
+      (tmux-title-of-emacs "Emacs"))
+    (when (and (getenv "TMUX")
+               (not noninteractive)
+               (not (string-match-p
+                     (concat  "^[0-9]+: " tmux-title-of-emacs)
+                     (shell-command-to-string "tmux lsw"))))
+      (setq my-saved-tmux-window-name
+            (let ((line (shell-command-to-string "tmux lsw|grep '(active)'")))
+              (string-match "[0-9]+: \\(.*\\)\\*" line)
+              (match-string 1 line)))
+      (shell-command (format "tmux rename-window '%s'" tmux-title-of-emacs))
+      (add-hook 'kill-emacs-hook
+                (lambda ()
+                  (when my-saved-tmux-window-name
+                    (shell-command (format "tmux rename-window '%s'" my-saved-tmux-window-name)))))))
+
+
 ;; 非常に重要な設定
 (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
 (setq custom-file "~/.emacs.d/custom.el")
@@ -2118,29 +2140,6 @@ If universal argument (C-u) is given, jump to the buffer."
 (eval-after-load 'package
   #'(progn
       (set-face-foreground 'package-name "aquamarine")))
-
-
-;;; tmux 内にいる && Emacs がインタラクティブに起動 && Emacs が window-name にないとき
-;;; window-name を設定する。
-;;; また、Emacs 終了時に window-name を起動時のものに戻す。
-(defvar my-saved-tmux-window-name nil)
-(let ((case-fold-search nil)            ; case-sensitive
-      (tmux-title-of-emacs "Emacs"))
-    (when (and (getenv "TMUX")
-               (not noninteractive)
-               (not (string-match-p
-                     (concat  "^[0-9]+: " tmux-title-of-emacs)
-                     (shell-command-to-string "tmux lsw"))))
-      (setq my-saved-tmux-window-name
-            (let ((line (shell-command-to-string "tmux lsw|grep '(active)'")))
-              (string-match "[0-9]+: \\(.*\\)\\*" line)
-              (match-string 1 line)))
-      (shell-command (format "tmux rename-window '%s'" tmux-title-of-emacs))
-      (add-hook 'kill-emacs-hook
-                (lambda ()
-                  (when my-saved-tmux-window-name
-                    (shell-command (format "tmux rename-window '%s'" my-saved-tmux-window-name)))))))
-
 
 (provide 'init)
 ;;; init.el ends here

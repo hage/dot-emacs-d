bbb4f10f5849826230fe523c46719ac9638ca4ea
URA Takefumi <ura.takefumi@gmail.com>
2018-05-08 01:22:10 +0900

    string-face を明るめにしてみた

--- init.el
+++ init.el
@@ -2158,7 +2158,7 @@ If universal argument (C-u) is given, jump to the buffer."
 (set-face-foreground 'highlight nil)
 (set-face-background 'highlight "palegreen4")
 (set-face-background 'region "LightSteelBlue4")
-(set-face-foreground 'font-lock-string-face "MediumPurple2")
+(set-face-foreground 'font-lock-string-face "orchid1")
 (set-face-foreground font-lock-builtin-face "CornflowerBlue")
 (set-face-foreground font-lock-keyword-face "DeepSkyBlue")
 (set-face-bold font-lock-keyword-face t)

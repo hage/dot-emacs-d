501f8c2014187f8d06b53a36f1b385fdaf661471
URA Takefumi <ura.takefumi@gmail.com>
2018-06-01 01:01:34 +0900

    *Man ...* バッファを popwin 制御下においた

--- init.el
+++ init.el
@@ -884,6 +884,7 @@ Otherwise indent whole buffer."
   (push '("*xref*") popwin:special-display-config)
   (push '("*robe-doc*" :stick t :dedicated t :width .5 :height .5) popwin:special-display-config)
   (push '("*Messages*" :position bottom :dedicated t :height .3) popwin:special-display-config)
+  (push '("\\*Man .*" :regexp t :position right :stick t :width .5) popwin:special-display-config)
   )
 
 

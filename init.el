9b504eac57ddd9002440b76deac5a88cacf6f1fd
URA Takefumi <ura.takefumi@gmail.com>
2019-03-27 00:31:37 +0900

    ファイル名の suffix が*.html.* であるものを web-mode に

--- init.el
+++ init.el
@@ -1775,7 +1775,7 @@ Otherwise sends the whole buffer."
   (add-to-list 'auto-mode-alist '("\\.as[cp]x$"   . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb$"       . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?$"     . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.html.eex?$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html\\.[^.]+$" . web-mode))
 
   (eval-after-load "web-mode"
     #'(progn

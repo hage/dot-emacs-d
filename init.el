99fd1fa7ab4206078a8ac2e858b27386a26be6e9
URA Takefumi <ura.takefumi@gmail.com>
2019-05-07 17:33:59 +0900

    [feat] ### title のようなスタイルに変更
    元々は「### title ###」のようなスタイルだった

--- init.el
+++ init.el
@@ -1859,6 +1859,8 @@ Otherwise sends the whole buffer."
     (define-key markdown-mode-map "\C-cts" 'markdown-insert-header-setext-1)
     (define-key markdown-mode-map "\C-ctt" 'markdown-insert-header-setext-2)
 
+    (setq markdown-asymmetric-header t) ; ### title のようなスタイル
+
     (setq my-another-markdown-previewer (if osxp "/Applications/Markn.app/Contents/MacOS/Electron"
                                           nil))
 

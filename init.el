0409716d8fd3e0570f1e2ad074436aafcfa55fb1
URA Takefumi <ura.takefumi@gmail.com>
2019-07-13 14:28:38 +0900

    [feat] バッファがどれか一つのスクリーンに強制所属をやめた

--- init.el
+++ init.el
@@ -2243,12 +2243,6 @@ Otherwise sends the current line."
 ;;; elscreen
 ;;;
 (when (require 'elscreen nil t)
-
-  (when (require 'elscreen-buffer-group nil t)
-    (setq elscreen-buffer-group-exclusive nil) ; t で buffer は一つのスクリーンにのみ存在できる
-    )
-
-
   (global-unset-key (kbd "M-t"))
   (custom-set-variables'(elscreen-prefix-key (kbd "M-t"))
                        '(elscreen-tab-display-kill-screen nil) ; タブの先頭に[X]を表示しない

37cd281ddcc08a16326c3758833547e0a9cf0bb7
URA, Takefumi <ura.takefumi@gmail.com>
2020-10-30 02:51:45 +0900

    [feat] php-modeの調整

--- init.el
+++ init.el
@@ -1870,12 +1870,15 @@ C-u を前置したときはどのような場合でも helm-buffers-list を起
 
   (defun my-on-hook-php-mode ()
     (company-mode t)
-    (require 'company-php)
-    (make-local-variable 'company-backends)
-    (setq company-backends '((company-tabnine company-ac-php-backend)
-                             company-bbdb company-eclim company-semantic company-files
-                             (company-dabbrev-code company-gtags company-etags company-keywords)))
-    ;(add-to-list 'company-backends 'company-ac-php-backend)
+    (setq lsp-enable-indentation nil)
+    (php-set-style "pear")
+    ;; (require 'company-php)
+    ;; (make-local-variable 'company-backends)
+    ;; (setq company-backends '((company-tabnine company-ac-php-backend)
+    ;;                          company-bbdb company-eclim company-semantic company-files
+    ;;                          (company-dabbrev-code company-gtags company-etags company-keywords)))
+                                        ;(add-to-list 'company-backends 'company-ac-php-backend)
+    (editorconfig-apply)
     )
   (add-hook 'php-mode-hook #'my-on-hook-php-mode))
 

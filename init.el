48153be9366a9e5b625612a33f5272caf0456413
URA, Takefumi <ura.takefumi@gmail.com>
2021-03-27 01:39:03 +0900

    [feat] enable smartparens in web-mode

--- init.el
+++ init.el
@@ -458,9 +458,11 @@
   :added "2021-03-23"
   :url "https://github.com/Fuco1/smartparens"
   :ensure t
-  :hook emacs-lisp-mode-hook
+  :hook emacs-lisp-mode-hook web-mode-hook
   :defer-config (require 'smartparens-config)
   :bind ((smartparens-mode-map
+          ("M-C-k" . kill-line)
+          ("C-k" . sp-kill-hybrid-sexp)
           ("M-R" . sp-raise-sexp)
           ("M-D" . sp-splice-sexp)
           ("M-H" . sp-backward-barf-sexp)

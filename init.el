a8eebf922ad214c4592d7d3683158a9dffcd185c
URA, Takefumi <ura.takefumi@gmail.com>
2021-04-22 18:11:02 +0900

    [feat] key bind M-t M-l to persp-switch

--- init.el
+++ init.el
@@ -758,6 +758,7 @@
           ("M-t s"   . persp-state-save)
           ("M-t k"   . persp-kill)
           ("M-t c"   . persp-switch)
+          ("M-t M-l" . persp-switch)
           ("C-x b"   . persp-ivy-switch-buffer)
           ("C-x C-b" . counsel-switch-buffer)
           ("M-t 0"   . my-persp-switch-to-main))))
